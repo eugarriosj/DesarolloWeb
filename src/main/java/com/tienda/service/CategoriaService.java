77422d7ccfac4dfaa825fd0d4d4297e40368d729
eugar <eugar@DESKTOP-A40FJBB>
2026-04-07 19:12:19 -0600

    service

---
+++ src/main/java/com/tienda/service/CategoriaService.java
@@ -0,0 +1,82 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tienda.service;
+
+import com.tienda.repository.CategoriaRepository;
+import com.tienda.domain.Categoria;
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class CategoriaService {
+    
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+    
+    @Autowired
+    private FirebaseStorageService firebaseStorageService;
+    
+    @Transactional(readOnly=true)
+    public List<Categoria> getCategorias(boolean activo) {
+        if (activo) {
+            return categoriaRepository.findByActivoTrue();
+        }
+        return categoriaRepository.findAll();
+    }
+    
+    @Transactional(readOnly=true)
+    public Optional<Categoria> getCategoria(Integer idCategoria) {
+        return categoriaRepository.findById(idCategoria);
+    }
+    
+    @Transactional
+    public void save(Categoria categoria, MultipartFile imagenFile) {
+        categoria = categoriaRepository.save(categoria);
+        if (imagenFile.isEmpty())
+            return;
+        try {
+            String rutaImagen = firebaseStorageService.uploadImage(imagenFile,
+                    "categoria",
+                    categoria.getIdCategoria());
+            categoria.setRutaImagen(rutaImagen);
+            categoriaRepository.save(categoria);
+        } catch (IOException e) {
+        }
+    }
+    
+    @Transactional
+    public void delete(Integer idCategoria) {
+        if (!categoriaRepository.existsById(idCategoria)) {
+            throw new IllegalArgumentException("La categoria con ID " + idCategoria + " no existe");
+        }
+        try {
+            categoriaRepository.deleteById(idCategoria);
+        } catch (DataIntegrityViolationException e) {
+            throw new IllegalStateException("No se puede eliminar la categoria, tiene datos asociados", e);
+        }
+    }
+    
+    
+    @Transactional(readOnly = true)
+    public List<Categoria> consultaDerivadaCategoria(String textoDescripcion) {
+        return categoriaRepository.findByActivoTrueAndDescripcionContaining(textoDescripcion);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Categoria> consultaCategoriaJPQL(String textoDescripcion) {
+        return categoriaRepository.consultaCategoriaJPQL(textoDescripcion);
+    }
+    @Transactional(readOnly = true)
+    public List<Categoria> consultaCategoriaSQL(String textoDescripcion) {
+        return categoriaRepository.consultaCategoriaSQL(textoDescripcion);
+    }
+
+}
